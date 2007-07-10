3c75a76f5057e49eb424bbfbc64e4ebb739c1e1d
steven <steven>
2007-07-10 21:02:46 +0000

    Rails simple localization plugin
    Localized application extension feature:
    - Renamed the "localized application string" feature to "localized application
      extension" because it's methods are added to the String class as well as to
      the Symbol class.
    - Documented the l method of this feature.
    
    Localized application feature:
    - Added an lc method to the ActionController::Base class. This method can be
      called directly in any controller class and will automatically prefix the
      specified keys with the controller name.
    - Updated the source documentation of this feature.
    - Cleaned out old code.

--- lib/features/localized_application.rb
+++ lib/features/localized_application.rb
@@ -71,60 +71,75 @@ module ArkanisDevelopment::SimpleLocalization #:nodoc:
       # Class variable to hold the scope stack of the +app_with_scope+ method.
       @@app_scope_stack = []
       
+      # Basically the same as the +app_not_scoped+ method but +app_scoped+ does
+      # respect the scope set by the +app_with_scope+ method.
+      # 
+      # Assuming the following language file data:
+      # 
+      #   app_default_value: No translation available
+      #   app:
+      #     index:
+      #       title: Welcome to XYZ
+      #       subtitle: Have a nice day...
+      # 
+      # The following code would output:
+      # 
+      #   Language.app_with_scope :index do
+      #     Language.app_scoped :title            # => "Welcome to XYZ"
+      #     Language.app_scoped :subtitle         # => "Have a nice day..."
+      #     Language.app_scoped "I don't exist"   # => "I don't exist"
+      #   end
+      #   
+      #   Language.app_scoped :index, :title    # => "Welcome to XYZ"
+      #   Language.app_scoped :not_existing_key # => "No translation available"
+      # 
       def app_scoped(*keys)
         self.app_not_scoped(*(@@app_scope_stack.flatten + keys))
       end
       
+      # This class method is used to access entries used by the localized
+      # application feature. Since the +app+ section of the language file is
+      # reserved for this feature this method restricts the scope of the entries
+      # available to the +app+ section. The method should only be used for
+      # application localization and therefor there is no need to access other
+      # sections of the language file with this method.
+      # 
+      #   app_default_value: No translation available
+      #   app:
+      #     index:
+      #       title: Welcome to XYZ
+      #       subtitle: Have a nice day...
+      # 
+      #   Language.app_not_scoped(:index, :subtitle) # => "Have a nice day..."
+      # 
+      # If the specified entry does not exists a default value is returned. If
+      # the last argument specified is a string this string is returned as
+      # default value. Assume the same language file data as above:
+      # 
+      #   Language.app_not_scoped(:index, "Welcome to my app") # => "Welcome to my app"
+      # 
+      # The <code>"Welcome to my app"</code> entry doesn't exists in the
+      # language file. Because the last argument is a string it will returned as
+      # a default value. If the last argument isn't a string the method will
+      # return the +app_default_value+ entry of the language file. Again, same
+      # language file data as above:
+      # 
+      #   Language.app_not_scoped(:index, :welcome) # => "No translation available"
+      # 
+      # The <code>:welcome</code> entry does not exists. The last argument isn't
+      # a string and therefore the value of the +app_default_value+ entry is
+      # returned. If this fall back entry does not exists +nil+ is returned.
+      # 
+      # This method does not respect the scope set by the +with_app_scope+
+      # method. This is done by the +app_scoped+ method.
       def app_not_scoped(*keys)
         self.entry(:app, *keys) || begin
           keys.last.kind_of?(String) ? keys.last : self.entry(:app_default_value)
         end
       end
       
-      def with_app_scope(*scope_sections, &block)
-        @@app_scope_stack.push scope_sections
-        begin
-          yield
-        ensure
-          @@app_scope_stack.pop
-        end
-      end
-      
-      # Added aliases for backward compartibility (pre 2.4 versions).
-      alias_method :app, :app_scoped
-      alias_method :app_with_scope, :with_app_scope
-      
-      # The +app+ class method will act like the
-      # ArkanisDevelopment::SimpleLocalization::Language#[] operator but
-      # restricts the scope on the +app+ section of the language file. The
-      # method should be used for application localization and therefor there
-      # is no need to access other sections of the language file with this
-      # method.
-      # 
-      #   app:
-      #     index:
-      #       title: Wellcome to XYZ
-      #       subtitle: Have a nice day...
-      # 
-      #   Language.app(:index, :subtitle) # => "Have a nice day..."
-      # 
-      # You can restrict the scope even further by using the +app_with_scope+
-      # method.
-      # 
-      #   Language.app_with_scope :index
-      #   Language.app :subtitle # => "Have a nice day..."
-      # 
-=begin
-      def app(*keys)
-        @@app_scope ||= []
-        self.entry(:app, *(@@app_scope + args)) || begin
-          keys.last.kind_of?(String) ? keys.last : self.entry(:app_default_value)
-        end
-      end
-=end
-      
-      # Narrows down the scope of the +app+ method. Useful if you have a very
-      # nested language file:
+      # Narrows down the scope of the +app_scoped+ method. Useful if you have a
+      # very nested language file and don't want to use the +lc+ helpers:
       # 
       #   app:
       #     layout:
@@ -134,40 +149,35 @@ module ArkanisDevelopment::SimpleLocalization #:nodoc:
       #           contact: Contact
       #           about: About
       # 
-      # Usually the calls to the +app+ method would look like this:
+      # Usually the calls to the +app_scoped+ method would look like this:
       # 
-      #   Language.app :layout, :nav, :main, :home     # => "Homepage"
-      #   Language.app :layout, :nav, :main, :contact  # => "Contact"
-      #   Language.app :layout, :nav, :main, :about    # => "About"
+      #   Language.app_scoped :layout, :nav, :main, :home     # => "Homepage"
+      #   Language.app_scoped :layout, :nav, :main, :contact  # => "Contact"
+      #   Language.app_scoped :layout, :nav, :main, :about    # => "About"
       # 
-      # In this situation you can use +app_with_scope+ to save some work:
+      # In this situation you can use +with_app_scope+ to save some work:
       # 
-      #   Language.app_with_scope :layout, :nav, :main do
-      #     Language.app :home     # => "Homepage"
-      #     Language.app :contact  # => "Contact"
-      #     Language.app :about    # => "About"
+      #   Language.with_app_scope :layout, :nav, :main do
+      #     Language.app_scoped :home     # => "Homepage"
+      #     Language.app_scoped :contact  # => "Contact"
+      #     Language.app_scoped :about    # => "About"
       #   end
       # 
-      # Every call to the +app+ method inside the block will automaticaly
-      # prepended with the language file sections you specified to
-      # +app_with_scope+.
-      # 
-      # PLEASE NOTE: It's currently not possible to nest +app_with_scope+
-      # calls. If no block is specified the scope will be set directly and you
-      # will have to reset it by your own.
-=begin
-      def app_with_scope(*scope_sections)
-        @@app_scope_stack
-        if block_given?
-          old_scope = @@app_scope
-          @@app_scope = scope_sections
+      # Every call to the +app_scoped+ method inside the block will
+      # automatically be prefixed with the sections you specified to the
+      # +with_app_scope+ method.
+      def with_app_scope(*scope_sections, &block)
+        @@app_scope_stack.push scope_sections
+        begin
           yield
-          @@app_scope = old_scope
-        else
-          @@app_scope = scope_sections
+        ensure
+          @@app_scope_stack.pop
         end
       end
-=end
+      
+      # Added aliases for backward compatibility (pre 2.4 versions).
+      alias_method :app, :app_scoped
+      alias_method :app_with_scope, :with_app_scope
       
     end
     
@@ -187,16 +197,49 @@ module ArkanisDevelopment::SimpleLocalization #:nodoc:
       
     end
     
-    # Localization helpers for the use in controllers only. This module will be
-    # mixed into ActionController::Base.
+    # Localization helpers for the use in controller classes only (not in
+    # actions!). This module will extend the ActionController::Base class.
     module ControllerHelpers
+      
+      # A more specialized version of the <code>GlobalHelpers#l</code> method
+      # which returns the localization information at the specified sections
+      # but prefixes the sections with the name of the current controller.
+      # 
+      # The main purpose of this method is to avoid unnecessary repetition of
+      # parameters to the <code>GlobalHelpers#l</code> method.
+      # 
+      # Assume this language file data:
+      # 
+      #   app:
+      #     about:
+      #       area_title: About this app...
+      #         
+      # 
+      # and that the following code is written directly in the +AboutController+
+      # class:
+      # 
+      #   lc :area_title # => 'About this app...'
+      #   l :about, :area_title # => 'About this app...'
+      # 
+      # Based on it's current context (the +about+ controller) the lc method
+      # automatically added the <code>:about</code> scope.
+      def lc(*sections)
+        ArkanisDevelopment::SimpleLocalization::Language.app_not_scoped(*([self.controller_name] + sections))
+      end
+      
+    end
+    
+    # Localization helpers for the use in controllers only. This module will be
+    # mixed into ActionController::Base and therefore be available to actions
+    # (as an instance method).
+    module ControllerActionHelpers
       
       # A more specialized version of the <code>GlobalHelpers#l</code> method
       # which returns the localization information at the specified sections
       # but prefixes the sections with the name of the current controller and
       # action.
       # 
-      # The main purpose of this method is to avoid unnecessary repeatition of
+      # The main purpose of this method is to avoid unnecessary repetition of
       # parameters to the <code>GlobalHelpers#l</code> method.
       # 
       # Assume this language file data:
@@ -211,6 +254,8 @@ module ArkanisDevelopment::SimpleLocalization #:nodoc:
       #   lc :title # => 'About...'
       #   l :about, :index, :title # => 'About...'
       # 
+      # Based on it's current context (+about+ controller and +index+ action)
+      # the lc method automatically added the <code>:about, :index</code> scope.
       def lc(*sections)
         ArkanisDevelopment::SimpleLocalization::Language.app_not_scoped(*([self.controller_name, self.action_name] + sections))
       end
@@ -225,7 +270,7 @@ module ArkanisDevelopment::SimpleLocalization #:nodoc:
       # which returns the localization information at the specified sections
       # but prefixes the sections with the name of the current template.
       # 
-      # The main purpose of this method is to avoid unnecessary repeatition of
+      # The main purpose of this method is to avoid unnecessary repetition of
       # parameters to the <code>GlobalHelpers#l</code> method.
       # 
       # Assume this language file data:
@@ -241,21 +286,23 @@ module ArkanisDevelopment::SimpleLocalization #:nodoc:
       #   lc :title # => 'About...'
       #   l :about, :index, :title
       # 
+      # The lc method automatically added the :about, :index prefix based on the
+      # name of the current view.
+      # 
       # Please not that the leading underscore of partial templates is removed.
       # In the template <code>app/view/shared/_message.rhtml</code> the +lc+
       # method will prefix the sections with <code>:app, :shared,
       # :message</code>.
-      # 
       def lc(*sections)
-        current_template = path_and_extension(template_name).first
+        current_template = path_and_extension(template_file_name).first
         dir, file = File.split(current_template)
         prefix_sections = dir.split '/'
         prefix_sections << file.gsub(/^_/, '')
         ArkanisDevelopment::SimpleLocalization::Language.app_not_scoped(*(prefix_sections + sections))
       end
       
-      # Returns the name of the template (relative to the ActionView base dir)
-      # from which this method is called.
+      # Returns the file name of the template (relative to the ActionView base
+      # dir) from which this method is called.
       # 
       # The call stack is used to get information about the current template.
       # This is not the perfect way but currently the simplest and fastest.
@@ -268,7 +315,7 @@ module ArkanisDevelopment::SimpleLocalization #:nodoc:
       # 
       #   <%= template_name %> # => shared/_messages.rhtml
       # 
-      def template_name
+      def template_file_name
         template_path = caller.detect{|level| level.slice @base_path}
         template_path.slice Regexp.new(".*#{Regexp.escape(@base_path)}/(.*)\\:\\d+.*"), 1
       end
@@ -279,6 +326,8 @@ module ArkanisDevelopment::SimpleLocalization #:nodoc:
 end
 
 ArkanisDevelopment::SimpleLocalization::Language.send :extend, ArkanisDevelopment::SimpleLocalization::LocalizedApplication::Language
+
 Object.send :include, ArkanisDevelopment::SimpleLocalization::LocalizedApplication::GlobalHelpers
+ActionController::Base.send :extend, ArkanisDevelopment::SimpleLocalization::LocalizedApplication::ControllerHelpers
+ActionController::Base.send :include, ArkanisDevelopment::SimpleLocalization::LocalizedApplication::ControllerActionHelpers
 ActionView::Base.send :include, ArkanisDevelopment::SimpleLocalization::LocalizedApplication::TemplateHelpers
-ActionController::Base.send :include, ArkanisDevelopment::SimpleLocalization::LocalizedApplication::ControllerHelpers
---
+++ lib/features/localized_application_extensions.rb
@@ -0,0 +1,61 @@
+# = Localized application extensions
+
+module ArkanisDevelopment::SimpleLocalization #:nodoc:
+  module LocalizedApplicationExtensions #:nodoc:
+    
+    # A shortcut for the LocalizedApplication::GlobalHelpers#l method. It'll use
+    # the value of the string or the name of the symbol as the key to query. Any
+    # arguments supplied to this method will be used to format the entry.
+    # 
+    # Assume the following language file data:
+    # 
+    #   app:
+    #     welcome: Welcome to my page
+    #     footer: 'Done by %s'
+    #     Welcome to this test page: Willkommen auf dieser Testseite
+    #     'This page was created by %s': Diese Seite wurde von %s erstellt
+    # 
+    # And here's some sample code:
+    # 
+    #   :welcome.l        # => "Welcome to my page"
+    #   :footer.l 'Mr. X' # => "Done by Mr. X"
+    #   :unknown.l        # => nil
+    # 
+    # You can also use this method on strings. However editing the string would
+    # result in a new key and therefore a new entry in the language file. Once
+    # an entry is created please don't modify the string. Maintenance could
+    # become very annoying in this case.
+    # 
+    # However the value of the string will be used as a default value if the
+    # specified entry does not exist. This could come in quite handy sometimes.
+    # 
+    #   "Welcome to this test page".l           # => "Willkommen auf dieser Testseite"
+    #   "This page was created by %s".l 'Mr. X' # => "Diese Seite wurde von Mr. X erstellt"
+    #   "This text isn't localized".l           # => "This text isn't localized"
+    # 
+    def l(*format_args)
+      app_args = [self.to_s]
+      app_args << {:values => format_args} unless format_args.empty?
+      Language.app_scoped *app_args
+    end
+    
+    def lc(*format_args)
+      app_args = get_app_file_in_context.split '/'
+      app_args << self.to_s
+      app_args << {:values => format_args} unless format_args.empty?
+      Language.app_not_scoped *app_args
+    end
+    
+    protected
+    
+    def get_app_file_in_context
+      latest_app_file = caller.detect{|level| level.slice /#{Regexp.escape(RAILS_ROOT)}\/app\/(controllers|views)\//}
+      latest_app_file.gsub! /^#{Regexp.escape(RAILS_ROOT)}\/app\/(controllers|views)\//, ''
+      latest_app_file.gsub! /#{Regexp.escape(File.extname(latest_app_file))}$/, ''
+    end
+    
+  end
+end
+
+String.send :include, ArkanisDevelopment::SimpleLocalization::LocalizedApplicationExtensions
+Symbol.send :include, ArkanisDevelopment::SimpleLocalization::LocalizedApplicationExtensions
