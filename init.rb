fe2e79387c5ed8be3c8ed45f0eed8735bed75cff
steven <steven>
2007-09-06 00:48:42 +0000

    Rails simple localization plugin
    Languages:
    - Added the Swedish language file contributed by Jonas Gauffin.
    
    Features:
    - Created a feature to substitute the model and attribute name into the
      ActiveRecord error messages. This doesn't realy work right now.

--- init.rb
+++ init.rb
@@ -1,12 +1,10 @@
-# Load the Language and LangSectionProxy classes and at the end the base file.
-# It will define the simple_localization method which will do all necessary
-# stuff.
+# Load all source files in the lib directory.
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each do |lib_file|
   require lib_file
 end
 
 # Preload any features which have to be ready immediately so they can be used
-# by models which have observer attected to them (which causes them to be
+# by models which have observers attected to them (which causes them to be
 # loaded before the simple_localization call).
 # 
 # The list of preloaded modules can be modified by simply defining the
---
+++ lib/features/extended_error_messages.rb
@@ -0,0 +1,51 @@
+module ArkanisDevelopment::SimpleLocalization #:nodoc:
+  module ExtendedErrorMessages
+    
+    module MessageExtension
+      
+      attr_accessor :prefix_with_attribute
+      
+      def prefix_with_attribute?
+        @prefix_with_attribute
+      end
+      
+      def substitute!(base, attribute)
+        self.replace Language.substitute_entry(self, :model => base.class.localized_model_name) if base.class.respond_to?(:localized_model_name)
+        self_before_attribute_substitution = self
+        self.replace Language.substitute_entry(self, :attr => base.class.human_attribute_name(attribute))
+        self.prefix_with_attribute = true if self == self_before_attribute_substitution
+      end
+      
+    end
+    
+  end
+end
+
+class ActiveRecord::Errors
+  
+  alias_method :add_without_substitution, :add
+  
+  def add(attribute, msg = @@default_error_messages[:invalid])
+    msg.send :extend, ArkanisDevelopment::SimpleLocalization::ExtendedErrorMessages::MessageExtension
+    msg.substitute! @base, attribute
+    add_without_substitution(attribute, msg)
+  end
+  
+  def full_messages
+    full_messages = []
+    
+    @errors.each_key do |attr|
+      @errors[attr].each do |msg|
+        next if msg.nil?
+        
+        if attr == 'base' or not msg.prefix_with_attribute?
+          full_messages << msg
+        else
+          full_messages << @base.class.human_attribute_name(attr) + " " + msg
+        end
+      end
+    end
+    full_messages
+  end
+  
+end
