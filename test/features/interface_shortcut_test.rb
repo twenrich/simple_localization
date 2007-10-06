81b3e080a0dc93430279195b8f0c6eb33fd19995
steven <steven>
2007-10-06 13:23:41 +0000

    Rails simple localization plugin
    Languages:
    - Renamed the Brazil language file to pt-br.yml.
    
    Tests:
    - Fixed a bug in the interface_shortcut_test causing other feature tests to
      fail when run by the test:features rake task.
    - Made the localized_model_attributes_test independent from the
      localized_column_human_name feature.

--- test/features/interface_shortcut_test.rb
+++ test/features/interface_shortcut_test.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 
 # Init SimpleLocalization with just the localized_date_and_time feature
 # activated.
-simple_localization :lang_file_dir => LANG_FILE_DIR, :languages => [:en, :de], :only => :interface_shortcut
+simple_localization :lang_file_dir => LANG_FILE_DIR, :languages => [:de, :en], :only => :interface_shortcut
 
 class InterfaceShortcutTest < Test::Unit::TestCase
   
@@ -15,6 +15,10 @@ class InterfaceShortcutTest < Test::Unit::TestCase
     assert_equal :en, Localization.used
     assert_equal :en, ArkanisDevelopment::SimpleLocalization::Language.used
     assert_equal ArkanisDevelopment::SimpleLocalization::Language.entry(:about, :language), Localization.entry(:about, :language)
+  end
+  
+  def teardown
+    ArkanisDevelopment::SimpleLocalization::Language.use LANG_FILE
   end
   
 end
--- test/features/localized_model_attributes_test.rb
+++ test/features/localized_model_attributes_test.rb
@@ -13,7 +13,6 @@ class Dinosaur < ActiveRecord::Base
   def self.columns() @columns ||= []; end
   def self.column(name, sql_type = nil, default = nil, null = true)
     column = ActiveRecord::ConnectionAdapters::Column.new(name.to_s, default, sql_type.to_s, null)
-    column.model_class = self
     columns << column
     column
   end
