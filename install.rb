b8cd632706abc7cb63451a5fc1612b4322332633
steven <steven>
2008-01-05 01:27:58 +0000

    Rails simple localization plugin
    Plugin:
    - Tried to fix the installation script by not using the RAILS_ROOT constant.
    - Set the version number to 2.4.1.

--- install.rb
+++ install.rb
@@ -1,4 +1,4 @@
 # Create an initializer for this plugin
-File.open "#{RAILS_ROOT}/config/initializers/simple_localization.rb" do |f|
+File.open "#{File.dirname(__FILE__)}/../../../config/initializers/simple_localization.rb" do |f|
   f.write 'simple_localization :language => :en'
 end
